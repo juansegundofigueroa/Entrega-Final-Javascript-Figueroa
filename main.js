73c1a8d96af1edbefca66f08f6a4ecd9652bcb06
juan segundo figueroa <juansegundo.figueroa@hotmail.com>
2024-03-03 13:36:20 -0300

    Tercer Preentrega Juan Segundo Figueroa

--- main.js
+++ main.js
@@ -1,27 +1,75 @@
-//productos
-class PRODUCTO{
-    constructor(id,nombre,descripcion,precio,imagen){
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.imagen = imagen;
-    }
-}
-
 const PRODUCTOS = [
-    new PRODUCTO(1,`Cupcake`,`masa de chocolate o vainilla con crema en la parte superior`,625,`./Productos/cupcakes.jpg`),
-    new PRODUCTO(2,`Tarta de moras`,`masa de chocolate glaseada con moras en la parte superior`,1275,`./Productos/Tarta de moras.jpg`),
-    new PRODUCTO(3,`Rosquillas`,`masa de vainilla frita glaseada con distintos sabores`,120,`./Productos/roscas.jpg`),
-    new PRODUCTO(4,`Roll de Canela`,`masa con sabor a canela,glaseada y decorada con distintos toppings`,250,`./Productos/rollos de canela.jpg`),
-    new PRODUCTO(5,`Pan`,`clasico de harina refinada o integral`,600,`./Productos/pan.jpg`),
-    new PRODUCTO(6,`Macarons`,`masa fina de distintos sabores y rellenos`,550,`./Productos/macarons.jpg`),
-    new PRODUCTO(7,`"Libritos"`,`masa ojaldrada,fritos y de grasa`,50,`./Productos/libritos.jpg`),
-    new PRODUCTO(8,`Cookies`,`distintos sabores y masas de chocolate o vainilla,todas glasedas`, 150, `./Productos/galletas.jpg`)
+    {id:1,
+    nombre:`Cupcake`,
+    descripcion:`masa de chocolate o vainilla con crema en la parte superior`,
+    precio:625,
+    imagen:`./Productos/cupcakes.jpg`,
+    miniatura: `./Miniaturas/Cupcakes.jpg`
+},
+    
+    {id:2,
+    nombre:`Tarta de moras`,
+    descripcion:`masa de chocolate glaseada con moras en la parte superior`,
+    precio:1275,
+    imagen:`./Productos/Tarta de moras.jpg`,
+    miniatura: `./Miniaturas/Tarta de moras.jpg`
+},
+    
+    {id:3,
+    nombre:`Rosquillas`,
+    descripcion:`masa de vainilla frita glaseada con distintos sabores`,
+    precio:120,
+    imagen:`./Productos/roscas.jpg`,
+    miniatura: `./Miniaturas/roscas.jpg`
+},
+    
+    {id:4,
+    nombre:`Roll de Canela`,
+    descripcion:`masa con sabor a canela,glaseada y decorada con distintos toppings`,
+    precio:250,
+    imagen:`./Productos/rollos de canela.jpg`,
+    miniatura: `./Miniaturas/rollos de canela.jpg`
+},
+    
+    {id:5,
+    nombre:`Pan`,
+    descripcion:`clasico de harina refinada o integral`,
+    precio:600,
+    imagen:`./Productos/pan.jpg`,
+    miniatura: `./Miniaturas/pan.jpg`
+},
+    
+    {id:6,
+    nombre:`Macarons`,
+    descripcion:`masa fina de distintos sabores y rellenos`,
+    precio:550,
+    imagen:`./Productos/macarons.jpg`,
+    miniatura: `./Miniaturas/macarons.jpg`
+},
+    
+    {id:7,
+    nombre:`"Libritos"`,
+    descripcion:`masa ojaldrada,fritos y de grasa`,
+    precio:50,
+    imagen:`./Productos/libritos.jpg`,
+    miniatura: `./Miniaturas/libritos.jpg`
+},
+    
+    {id:8,
+    nombre:`Cookies`,
+    descripcion:`distintos sabores y masas de chocolate o vainilla,todas glasedas`,
+    precio:150,
+    imagen:`./Productos/galletas.jpg`,
+    miniatura: `./Miniaturas/galletas.jpg`
+}
 ]
 // funciones
-const contenedorProductos = document.getElementById("contenedorDeProdcutos");
-function agregarCards(PRODUCTOS){
+const elementosCarrito = JSON.parse(localStorage.getItem('carrito'))||[];
+const contenedorProductos = document.getElementById("contenedorDeProductos");
+const contenedorCarrito = document.getElementById('AgregadosAlCarrito');
+const totalPrecio = document.getElementById('total');
+
+function agregarCards(){
     PRODUCTOS.forEach(PRODUCTO =>{
         const card = document.createElement("div");
         card.classList.add("card");
@@ -30,113 +78,77 @@ function agregarCards(PRODUCTOS){
                             <img src="${PRODUCTO.imagen}" alt="${PRODUCTO.nombre}">
                             <p>${PRODUCTO.descripcion}</p>
                             <p>Precio por Unidad: $${PRODUCTO.precio}</p>
-                            <button>Agregar</button>
+                            <button class="btn" data-id="${PRODUCTO.id}">Agregar</button>
                             `
-    contenedorDeProductos.appendChild(card);
+    contenedorProductos.appendChild(card);
     })
 }
+
+function agregarAlCarrito(idPRODUCTO){
+    const item = elementosCarrito.find(item => item.id === idPRODUCTO);
+    if(item){
+        item.cantidad++
+    }else{
+        const producto = PRODUCTOS.find(p => p.id === idPRODUCTO);
+        if(producto){
+            elementosCarrito.push({...producto, cantidad:1});
+        }
+    }
+    guardarCarrito();
+    renderizarCarrito();
+}
+
+function eliminarDelCarrito(idProducto){
+    const indice = elementosCarrito.findIndex(item => item.id === idProducto);
+    if(indice !== -1){
+        elementosCarrito.splice(indice, 1);
+        guardarCarrito();
+        renderizarCarrito();
+    }
+}
+
+function guardarCarrito() {
+    localStorage.setItem('carrito', JSON.stringify(elementosCarrito));
+}
+
+function renderizarCarrito() {
+    contenedorCarrito.innerHTML = '';
+    let precioTotal = 0;
+    elementosCarrito.forEach(item =>{
+        const div = document.createElement('div');
+        const p = document.createElement('p');
+        const img = document.createElement('img');
+            img.src = `${item.miniatura}`;
+            div.classList.add('itemDeLaLista');
+            p.textContent = `${item.nombre} x ${item.cantidad}
+            $${item.precio * item.cantidad}`
+            const btnEliminar = document.createElement('button');
+            btnEliminar.textContent = 'Eliminar';
+            btnEliminar.addEventListener('click', () => eliminarDelCarrito(item.id))
+            contenedorCarrito.appendChild(div);
+            div.appendChild(img);
+            div.appendChild(p);
+            div.appendChild(btnEliminar);
+            precioTotal += item.precio * item.cantidad;
+    })
+    totalPrecio.textContent = precioTotal;
+}
+
+renderizarCarrito();
+
+function realizarCompra(){
+    alert(`El total de su compra es de $${totalPrecio.textContent}`);
+    elementosCarrito.length = 0;
+    renderizarCarrito();
+}
+
+document.getElementById('btndecomprar').addEventListener('click', realizarCompra);
+
+contenedorProductos.addEventListener('click',function(evento){
+    if(evento.target.classList.contains('btn')){
+        const idProducto = parseInt(evento.target.getAttribute('data-id'));
+        agregarAlCarrito(idProducto);
+    }
+});
+
 agregarCards(PRODUCTOS);
-function ordenarAZ(){
-    PRODUCTOS.sort(
-        function(a,b){
-        const nombreA = a.nombre.toLocaleLowerCase();
-        const nombreB = b.nombre.toLowerCase();
-        if (nombreA < nombreB) {
-            return -1;}
-            if(nombreA > nombreB) {
-                return 1
-                }
-        return 0;
-    }
-    )
-}
-function ordenarZA(){
-    PRODUCTOS.sort(
-        function(a,b){
-        const nombreA = a.nombre.toLocaleLowerCase();
-        const nombreB = b.nombre.toLowerCase();
-        if (nombreA > nombreB) {
-            return -1;}
-            if(nombreA < nombreB) {
-                return 1
-                }
-        return 0;
-    }
-    )
-}
-function ordenarPorMayorPrecio(){
-PRODUCTOS.sort(function(a,b){
-    return b.precio - a.precio;
-})
-}
-function ordenarPorMenorPrecio(){
-    PRODUCTOS.sort(function(a,b){
-        return a.precio - b.precio;
-    })
-}
-function agregarAlCarrito(carrito, productoID, cantidad) {
-    const producto = PRODUCTOS.find(item => item.id === productoID);
-    if (producto) {
-        if (carrito.hasOwnProperty(producto.nombre)) {
-            carrito[producto.nombre].cantidad += cantidad;
-            carrito[producto.nombre].precioTotal += cantidad * producto.precio;
-        } else {
-            carrito[producto.nombre] = {
-                precio: producto.precio,
-                cantidad: cantidad,
-                precioTotal: cantidad * producto.precio
-            };
-        }
-        console.log(`Se agregaron ${cantidad} ${producto.nombre}(s) al carrito.`);
-    } else {
-        console.log(`El producto con ID ${productoID} no fue encontrado.`);
-    }
-}
-function quitarDelCarrito(carrito, productoID, cantidad) {
-    const producto = PRODUCTOS.find(item => item.id === productoID);
-    if (producto) {
-        const nombreProducto = producto.nombre;
-        if (carrito.hasOwnProperty(nombreProducto)) {
-            if (cantidad >= carrito[nombreProducto].cantidad) {
-                delete carrito[nombreProducto];
-            } else {
-                carrito[nombreProducto].cantidad -= cantidad;
-                carrito[nombreProducto].precioTotal -= cantidad * carrito[nombreProducto].precio;
-            }
-            console.log(`Se eliminaron ${cantidad} ${nombreProducto}(s) del carrito.`);
-        } else {
-            console.log(`El producto ${nombreProducto} no está en el carrito.`);
-        }
-    } else {
-        console.log(`El producto con ID ${productoID} no fue encontrado.`);
-    }
-}
-function calcularPrecio(carrito) {
-    let precioTotal = 0;
-    for (const producto in carrito) {
-        precioTotal += carrito[producto].precioTotal;
-    }
-    return precioTotal;
-}
-function calcularPrecioTotal(carrito) {
-    let precioTotal = 0;
-    for (const producto in carrito) {
-        precioTotal += carrito[producto].precioTotal;
-    }
-    return precioTotal + precioTotal*0.21;
-}
-
-//ejecuciones
-
-let carrito = {};
-//carrito,numero de id,cantidada
-agregarAlCarrito(carrito, 5, 2);
-agregarAlCarrito(carrito, 3, 2);
-agregarAlCarrito(carrito, 5, 3);
-quitarDelCarrito(carrito, 5, 2);
-const precio = calcularPrecio(carrito);
-const precioTotal = calcularPrecioTotal(carrito);
-
-console.log("Carrito actualizado:", carrito);
-console.log("Precio a abonar",precio);
-console.log("Precio con iva agregado",precioTotal);
